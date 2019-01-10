d829f4d314712b6d95b941a896991c96a333965f
GideonBrasil <gidxx_z@yahoo.com>
2019-01-10 04:31:40 +0000

    Add chorusFor, chorusWhile and packingList

---
+++ packingList.js
@@ -0,0 +1,6 @@
+let packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
