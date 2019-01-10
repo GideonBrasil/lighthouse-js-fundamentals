f3864d002e8df8af7f112d5daec692e692a961d9
GideonBrasil <gidxx_z@yahoo.com>
2019-01-10 14:45:14 +0000

    Looping exercises.

---
+++ range.js
@@ -0,0 +1,17 @@
+function range(start, end, step) {
+  if (start === undefined || end === undefined || step === undefined || start > end || step < 0) {
+    return [];
+  }
+
+  var startNumber = start;
+  let someArray = [];
+  while (startNumber <= end) {
+    someArray.push(startNumber);
+    startNumber += step;
+  }
+  return (someArray);
+}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
